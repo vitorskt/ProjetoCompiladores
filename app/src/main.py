01ec9881d7721b2761b7d5818002e32014658c50
Vitor <vitorsoaresk@gmail.com>
2023-05-02 20:17:34 -0300

    refatoracao do codigo

--- app/src/main.py
+++ app/src/main.py
@@ -1,59 +1,54 @@
-def is_valid_token(token):
+vogais = "aeiou"
+consoantes = "bcdfglmnprstvxz"
+caracteres_validos = "123456789 "
+
+
+def is_valid_token(token, posicao):
     """
     Validates all tokens (letters that make up the word)
     :param token: letter
     :return: Boolean value (True if token is valid)
     """
-    valid_chars = "abcdefgilmnoprstuvxz0123456789 "
-    invalid_chars = "jwkyçhq/()&%$#@!"
-    if len(token) > 1:
-        return False
-    if token in invalid_chars:
-        return False
-    if token not in valid_chars:
-        return False
-    return True
 
-
-def is_valid_word(word):
-    """
-    checks if the input word is valid
-    :param word: word typed by the user
-    :return: Boolen value (True if is a valid world)
-    """
-    vogais = "aeiou"
-    consoantes_validas = "bcdfglmnprstvxz"
-
-    if word[0] in "xz":
+    if posicao == 0 and token in "xz":
         print("Palavras iniciadas com as consoantes X ou Z são palavras reservadas pelo sistema.")
         return False
-    if not word.isalnum(): # verifica se é alfanumerico (não aceita caracteres especiais) - regra 3
+
+    if posicao == 0 and token not in consoantes:
         return False
-    if word[0].lower() in vogais: # não pode começar com vogais. regra 5
+
+    if token not in vogais and token not in consoantes and token not in caracteres_validos:
         return False
-    for i in range(len(word) - 1): #sempre alternar entre consoante e vogal - regra 5.1
-        if word[i].lower() in vogais and word[i + 1].lower() in vogais:
-            return False
+
     return True
 
 
-def lexer(input_string):
-    print(f"cadeia inicial: {input_string}")
-    if len(input_string) > 10:
-        print("Cadeia com mais de 10 tokens, pegamos apenas os 10 primeiros.")
-        input_string = input_string[:10]
-    for token in input_string:
-        # print(token)
-        if not is_valid_token(token): # == False
-            return f"A cadeia é rejeitada. O token {token} não é válido."
-    #if not is_valid_word(word):
-    #    print("QQQ")
-    #    return f"A cadeia é rejeitada. A palavra {word} não é válida."
-    """if words[0][0].lower() in "aeiou":
-        return "A cadeia é rejeitada. A primeira palavra deve começar com uma consoante válida."
-    if words[-1][-1].isdigit():
-        return "A cadeia é aceita."
-    else:
-        return "A cadeia é rejeitada. A última palavra deve terminar com um algarismo numérico."
-    """
-lexer("zaralhei demais kkk")
+def lexer(cadeia):
+    print(f"cadeia inicial: {cadeia}")
+    cadeia = cadeia.lower()
+    if len(cadeia) > 10:
+        cadeia = cadeia[:10]
+        print(f"NOVA CADEIA: {cadeia}")
+    for posicao, token in enumerate(cadeia):
+        if not is_valid_token(token, posicao): # == False
+            print(f"A cadeia é rejeitada. O token {token} não é válido.")
+            return 0
+
+        if len(cadeia) - 1 != posicao and token in caracteres_validos.strip():
+            print("Cadeia Inválida 1")
+            return 0
+
+        elif len(cadeia) - 1 == posicao and token in caracteres_validos.strip():
+            print("CADEIA ACEITA")
+            return 0
+
+        if not (cadeia[posicao] in consoantes and cadeia[posicao + 1] in vogais) and \
+            not (cadeia[posicao] in vogais and cadeia[posicao + 1] in consoantes):
+
+            if len(cadeia) - 1 == posicao + 1 and cadeia[posicao + 1] in caracteres_validos.strip():
+                print("CADEIA ACEITA")
+            else:
+                print("Cadeia Inválida 2")
+            return 0
+    print("CADEIA ACEITA")
+lexer("paralazemi dematis lll")
